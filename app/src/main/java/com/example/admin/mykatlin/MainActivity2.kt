d601370f130641a63f1e3cacc32448af37dba6c7
583006809@qq.com <linzehao123>
2017-11-09 09:44:41 +0800

    代码提交

--- app/src/main/java/com/example/admin/mykatlin/MainActivity2.kt
+++ app/src/main/java/com/example/admin/mykatlin/MainActivity2.kt
@@ -1,16 +1,26 @@
 package com.example.admin.mykatlin
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.LinearLayout
+import com.example.admin.mykatlin.databinding.ActivityMainBinding
+import com.example.admin.mykatlin.MainActivity2.MyListener
+import java.util.*
+
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     val list = listOf(1, 2, 3, 4, 5, 6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+//        User user = new User("Test", "User");
+//        binding.setUser(user);
 
         list.forEachIndexed { index,
                               value ->
@@ -30,9 +40,59 @@ class MainActivity2 : AppCompatActivity() {
         val a: Int? = null
         a?.toString()
 
-        val myString = a?.toString() ?: throw IllegalStateException()
+//        val myString = a?.toString() ?: throw IllegalStateException()
 
 
+
+        val sum = { x: Int, y: Int -> x + y }
+
+        val sum1: (Int, Int) -> Int = { x, y -> x + y }
+
+
+        TestLamda() { a, b ->
+            Log.i("","")
+           ""
+        }
+//        {a,b->String}()
+//        TestLamda2({ a, b ->
+//            val result = a + b
+//            result
+//        })
     }
+
+    fun TestLamda(listener: (a: String, b :Int) -> String){
+        val a = "Lamda"
+        val b = 3
+        listener.let { Log.e("linzehao", it(a, b))}
+    }
+
+    fun TestLamda3(listener: (a: String, b :Int) -> String){
+        val a = "Lamda"
+        val b = 3
+        a+b
+    }
+
+    interface MyListener {
+        fun logHello(a: String, b: Int): String
+    }
+
+    fun TestLamda2(listener: MyListener) {
+        val a = "Lamda"
+        val b = 2
+        val result = listener.logHello(a, b)
+        Log.e("linzehao", result)
+    }
+
+
+
+    fun getTest(test2:test,test1:(test)->Unit){
+        test1(test2)
+    }
+
+    class test{
+        fun println( message:String){Log.i("linzehao",message)}
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/admin/mykatlin/MainActivity3.kt
@@ -0,0 +1,67 @@
+package com.example.admin.mykatlin
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import com.example.admin.mykatlin.databinding.ActivityMainBinding
+
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val list = listOf(1, 2, 3, 4, 5, 6)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        User user = new User("Test", "User");
+//        binding.setUser(user);
+
+
+        var list2 = listOf(1, 2, 4, 6)
+
+        TestLamda2(listener = object : MyListener {
+            override fun log(a: Int, b: Int): Int {
+                return a + b + a
+            }
+
+        })
+
+        binding.text1.setOnClickListener{  Log.i("linzehao","llll")}
+
+        setOnClickListener1{a,b->a+b+a}
+
+        TestLamda2(listener = object : MyListener {
+            override fun log(a: Int, b: Int): Int {
+                return a + b + a
+            }
+
+        })
+
+    }
+
+    fun TestLamda2(listener: MyListener) {
+        val a = 5
+        val b = 2
+        Log.i("linzehao", "" + listener.log(a, b))
+    }
+
+    interface MyListener {
+         fun log(a: Int, b: Int): Int
+    }
+
+    interface OnClickListener {
+        fun onClick(v: View)
+    }
+
+    fun setOnClickListener1(listener: (a:String,b:Int ) -> String){
+        var a :String = "s"
+        var b :Int  =1
+        listener.let { Log.i("linzehao", it(a, b))}
+    }
+
+
+}
+
